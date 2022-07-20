55d1f2fa8f483fcfeff4d5b6814d6dae4379c379
AhayahLit <lau1087054@gmail.com>
2022-07-19 18:32:28 -0700

    lat and long

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,63 @@
+let lat= "latitude";
+let long= "longitude";
+
 let weather = {
     apiKey: "ddca40accecca93e8c08c9f732720dec",
-    fetchWeather: function (city) {
+    fetchWeather: function(city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
              + city
              + "&units=metric&appid=" 
-             + this.appKey
-        )
+             + this.apiKey
+             );
+    },
+    fetchWeather: function(city) {
+    apiKey: "ddca40accecca93e8c08c9f732720dec",
+             fetch(
+            "http://api.openweathermap.org/geo/1.0/direct?q="
+            + lat
+            + "&limit="
+            + long
+            + this.apiKey 
+            )
         .then((response) => response.json())
         .then((data) => this.displayWeather(data));
-    },
+             },
+
     displayWeather: function(data) {
+        console.log(data);
         const {name} = data;
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        document.querySelector(".city").innerText = "Weather in" + name;
-        document.querySelector(".descripion").innerText = desctiption;
+        const {long}= data.longitude;
+        const {lat}= data.latitude;
+
+        document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".temp").innerText = temp;
-        document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed" + speed + "mph";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed " + speed + "mph";
         document.querySelector(".weather").classList.remove("loading");
+        document.querySelector(".latitude").innerText = lat; 
+        document.querySelector(".longitude").innerText= long; 
     }   
 };
-search: function (city) {
-    this.featchWeather(document.querySelector(".search-bar").value);
-}
-document.querySelector(".search-button")
-.addEventListener(".click", function (){
-    weather.search(); 
+// function search(city) {
+//     this.featchWeather(document.querySelector(".search-bar").value);
+// }
+document.querySelector("button")
+.addEventListener("click", function (){
+    var searchValue = document.querySelector("input");
+    console.log(searchValue.value);
+    if (searchValue.value == "") { 
+    alert("Please Enter a City");
+    }  
+    weather.fetchWeather(searchValue.value); 
 })
-document.querySelector(".search-bar")
-.addEventListener("keyup", function(event) {
-    if (event.key == "enter"); {
-        weather.search;
-    }
+// document.querySelector(".search-bar")
+// .addEventListener("keyup", function(event) {
+//     if (event.key == "enter"); {
+//         weather.search;
+//     }
 
-})
-weather.fetchWeather(Hollywood);
+// })
+//weather.fetchWeather();
